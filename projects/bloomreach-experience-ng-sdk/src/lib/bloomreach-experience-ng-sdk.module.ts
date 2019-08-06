853a84188267f6a445c9b193c4f41a77c12da9c1
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-08-06 11:24:16 +0200

    TRIVIAL Add missing HttpClientModule import

--- projects/bloomreach-experience-ng-sdk/src/lib/bloomreach-experience-ng-sdk.module.ts
+++ projects/bloomreach-experience-ng-sdk/src/lib/bloomreach-experience-ng-sdk.module.ts
@@ -16,6 +16,7 @@
 
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { ApiUrlsService } from './services/api-urls.service';
@@ -48,18 +49,23 @@ import { InitializeSdkService } from './services/initialize-sdk.service';
   entryComponents: [ UndefinedComponent ],
   imports: [
     CommonModule,
+    HttpClientModule,
     RouterModule,
   ],
   exports: [CmsEditButtonComponent, RenderCmsComponent ]
 })
 export class BloomreachExperienceNgSdkModule {
-
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: BloomreachExperienceNgSdkModule,
       providers: [
-        ApiUrlsService, ComponentMappingsService, ImageUrlService, InitializeSdkService, PageModelService, RequestContextService
-      ]
+        ApiUrlsService,
+        ComponentMappingsService,
+        ImageUrlService,
+        InitializeSdkService,
+        PageModelService,
+        RequestContextService,
+      ],
     };
   }
 }
